005f96147297fa6b9e5812f49dc65be08b030524
Rahul jangir <rahuljangir4764@gmail.com>
2026-04-19 17:58:03 +0530

    chore: Initialize Redux store configuration

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './slices/cartSlice';
+import productsReducer from './slices/productsSlice';
+
+/**
+ * Single Redux store combining cart and product catalog slices.
+ */
+export const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+    products: productsReducer,
+  },
+});
